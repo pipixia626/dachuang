8948df4cc9780ac9f15904740969681e6c0d58ff
zh-hang <zhangh127@163.com>
2021-05-05 23:52:53 +0800

    完成基本布局

---
+++ app/src/main/java/com/example/dachuang/ui/travel/TravelActivity.kt
@@ -0,0 +1,21 @@
+package com.example.dachuang.ui.travel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dachuang.R
+
+class TravelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_travel)
+        initView()
+        initData()
+    }
+
+    private fun initView(){
+
+    }
+
+    private fun initData(){}
+
+}
