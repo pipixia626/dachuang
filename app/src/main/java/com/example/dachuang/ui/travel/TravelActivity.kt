70867f4c5bfcae95f7db3945a8baf6d697e61709
zh-hang <zhangh127@163.com>
2021-05-06 10:13:23 +0800

    基本完成

--- app/src/main/java/com/example/dachuang/ui/travel/TravelActivity.kt
+++ app/src/main/java/com/example/dachuang/ui/travel/TravelActivity.kt
@@ -2,6 +2,8 @@ package com.example.dachuang.ui.travel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.example.dachuang.R
 
 class TravelActivity : AppCompatActivity() {
@@ -14,6 +16,21 @@ class TravelActivity : AppCompatActivity() {
 
     private fun initView(){
 
+    }
+
+    private fun initSpinner(){
+        val spinner: Spinner = findViewById(R.id.number_spinner)
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.number_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
     }
 
     private fun initData(){}
