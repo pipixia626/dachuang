e21d58b96d6e6de66ad1aa13918e29e2bcb7e640
pipixia626 <814151675@qq.com>
2021-05-06 10:16:48 +0800

    添加推荐结果界面，但尚未实现

--- app/src/main/java/com/example/dachuang/MainActivity.kt
+++ app/src/main/java/com/example/dachuang/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.dachuang
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 import com.example.dachuang.ui.login.LoginFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_main)
 
         val fragmentManager = supportFragmentManager
